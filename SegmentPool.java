ab45a88d19dce47a932736ed535fda7f7e823070
payal790 <ritu32kaur@gmail.com>
2024-06-08 23:13:09 +0530

    Add files via upload

---
+++ SegmentPool.java
@@ -0,0 +1,40 @@
+package dc.squareup.okio;
+
+final class SegmentPool {
+    static final long MAX_SIZE = 65536;
+    static long byteCount;
+    static Segment next;
+
+    private SegmentPool() {
+    }
+
+    static void recycle(Segment segment) {
+        if (segment.next != null || segment.prev != null) {
+            throw new IllegalArgumentException();
+        } else if (!segment.shared) {
+            synchronized (SegmentPool.class) {
+                long j = byteCount + 8192;
+                if (j <= MAX_SIZE) {
+                    byteCount = j;
+                    segment.next = next;
+                    segment.limit = 0;
+                    segment.pos = 0;
+                    next = segment;
+                }
+            }
+        }
+    }
+
+    static Segment take() {
+        synchronized (SegmentPool.class) {
+            Segment segment = next;
+            if (segment == null) {
+                return new Segment();
+            }
+            next = segment.next;
+            segment.next = null;
+            byteCount -= 8192;
+            return segment;
+        }
+    }
+}
